58b3e4f551a5086bebb568f59f8bd2a95b991602
TBTX <130815778+erikhinla@users.noreply.github.com>
2026-02-17 14:12:46 -0800

    Add ContextArchitecture route to App component

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import EvaParadis from "./pages/EvaParadis";
+import ContextArchitecture from "./pages/ContextArchitecture";
 import NotFound from "./pages/NotFound";
 
 const queryClient = new QueryClient();
@@ -19,6 +20,7 @@ const App = () => (
           <Route path="/" element={<Index />} />
           <Route path="/eva" element={<EvaParadis />} />
           <Route path="/eva-paradis" element={<EvaParadis />} />
+          <Route path="/contextarchitecture" element={<ContextArchitecture />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
         </Routes>
